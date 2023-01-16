577657a85581c042cfebef5c52e492c9f03281a9
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-01-16 17:54:31 +0100

    Add shrug command

---
+++ src/commands/util/shrug.ts
@@ -0,0 +1,29 @@
+import { MessageEvent, MessageEventContent } from "matrix-bot-sdk";
+import { CommandMatrixClient } from "../..";
+
+export default {
+  name: 'shrug',
+  description: 'Append ¯\\_(ツ)_/¯ to the text',
+  usage: ' [text]',
+  run: (roomId: string, event: MessageEvent<MessageEventContent>, args: string[], client: CommandMatrixClient) => {
+    let text = args.join(' ');
+    text += ' ¯\\_(ツ)_/¯';
+
+    return client.sendMessage(roomId, {
+      body: `* ${text}`,
+      msgtype: 'm.text',
+      'm.relates_to': {
+        rel_type: 'm.replace',
+        event_id: event.eventId,
+      },
+      format: 'org.matrix.custom.html',
+      formatted_body: `* ${text}`,
+      'm.new_content': {
+        'msgtype': 'm.text',
+        'body': text,
+        'format': 'org.matrix.custom.html',
+        'formatted_body': text
+      }
+    });
+  }
+}
