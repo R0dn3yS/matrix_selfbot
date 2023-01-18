28867837415a7a70f70a11e5d5d79b184e517978
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-01-18 15:39:00 +0100

    random httpcat

--- src/commands/util/httpcat.ts
+++ src/commands/util/httpcat.ts
@@ -8,9 +8,10 @@ export default {
   description: 'Get a HTTP cat',
   usage: ' [Status Code]',
   run: async (roomId: string, event: MessageEvent<MessageEventContent>, args: string[], client: CommandMatrixClient) => {
-    if (args[0] === undefined) return;
+    const codes = [100, 101, 102, 200, 201, 202, 203, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425, 426, 429, 431, 444, 450, 451, 498, 499, 500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525, 599];
+    const randomCode = codes[Math.ceil(Math.random() * codes.length)];
 
-    const code = args[0];
+    const code = args[0] === undefined ? randomCode.toString() : args[0] ;
 
     if (!existsSync(`httpcat/${code}.jpg`)) {
       downloadImage(`https://http.cat/${code}`, `httpcat/${code}.jpg`);
