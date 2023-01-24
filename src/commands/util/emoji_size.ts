263446c6bbe792ecec6295840cf919e0dce59149
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-01-24 14:45:04 +0100

    fix

--- src/commands/util/emoji_size.ts
+++ src/commands/util/emoji_size.ts
@@ -6,7 +6,24 @@ export default {
   description: 'Set the default emoji size',
   usage: ' [size]',
   run: async (roomId: string, event: MessageEvent<MessageEventContent>, args: string[], client: CommandMatrixClient) => {
-    let emojiSize = args[0] === '' ? client.emojiSize : parseInt(args[0]);
-    if (emojiSize === undefined) emojiSize = client.emojiSize;
+    let text = '';
+    
+    if (args[0] === undefined) {
+      text = `Emoji size is: ${client.emojiSize}`;
+    } else {
+      let emojiSize = args[0] === '' ? client.emojiSize : parseInt(args[0]);
+      if (emojiSize === undefined) emojiSize = client.emojiSize;
+  
+      client.emojiSize = emojiSize;
+
+      text = `Emoji size set to: ${emojiSize}`;
+    }
+
+    return client.sendMessage(roomId, {
+      body: text,
+      msgtype: 'm.text',
+      format: 'org.matrix.custom.html',
+      formatted_body: text,
+    });
   }
 }
