1ae155390ee4cf34746a88880c0becd9ef80d47e
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-01-24 16:06:07 +0100

    forgot await

--- src/commands/util/aw.ts
+++ src/commands/util/aw.ts
@@ -11,6 +11,6 @@ export default {
     const url = 'https://wiki.archlinux.org/index.php?search=' + args.join('+');
     const newUrl = await fetch(url).then(res => res.url);
 
-    return sendText(roomId, client, newUrl);
+    return await sendText(roomId, client, newUrl);
   }
 }
