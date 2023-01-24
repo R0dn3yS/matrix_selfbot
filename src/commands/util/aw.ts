21a634f72cff4e2dee4764f444924dd7cba6ae23
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-01-24 16:01:41 +0100

    another fix

--- src/commands/util/aw.ts
+++ src/commands/util/aw.ts
@@ -11,6 +11,6 @@ export default {
     const url = 'https://wiki.archlinux.org/index.php?search=' + args.join('+');
     const newUrl = await fetch(url).then(res => res.url);
 
-    return sendText(newUrl);
+    return sendText(roomId, client, newUrl);
   }
 }
