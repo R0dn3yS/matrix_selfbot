4e8831ea4aedbb305bccf98f7bd2259f2bb39d3e
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-01-30 02:19:03 +0100

    debug

---
+++ src/commands/util/debug.ts
@@ -0,0 +1,21 @@
+import { MessageEvent, MessageEventContent } from 'matrix-bot-sdk';
+import { CommandMatrixClient } from '../..';
+import { sendText } from '../../util/util';
+
+export default {
+  name: 'debug',
+  description: 'Return help menu',
+  usage: '',
+  admin: true,
+  run: async (roomId: string, event: MessageEvent<MessageEventContent>, args: string[], client: CommandMatrixClient) => {
+    let text = 'epic fail';
+
+    try {
+      text = `Value of <code>${args.join(' ')}</code> is set to: <code>${eval(args.join(' '))}</code>`;
+    } catch (e) {
+      console.log(e);
+    }
+
+    return await sendText(roomId, client, text);
+  }
+}
